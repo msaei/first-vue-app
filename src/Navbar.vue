e90a4e380a9b8dd5b244311cc3eaeb9ca4316197
Mehdi M. Saei <mahdi.saeikhalili@gmail.com>
2019-01-02 16:56:48 -0500

    nesting components

---
+++ src/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <a href>home</a>
+      </li>
+      <li>
+        <a href>about</a>
+      </li>
+      <li>
+        <a href>contact</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+nav {
+  text-align: center;
+}
+nav ul {
+  padding: 0;
+}
+nav li {
+  display: inline-block;
+  list-style-type: none;
+  margin: 0;
+}
+</style>
+
+
